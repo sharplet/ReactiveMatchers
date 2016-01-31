f26a02553446eb2038b8847b3fc9ff2ffa38a546
Adam Sharp <adsharp@me.com>
2016-01-31 17:45:12 +1100

    Add an overload of `complete()` for signals

--- Sources/CompleteMatcher.swift
+++ Sources/CompleteMatcher.swift
@@ -1,6 +1,22 @@
 import Nimble
 import ReactiveCocoa
 
+public func complete<Value, Error: ErrorType>() -> NonNilMatcherFunc<Signal<Value, Error>> {
+  return NonNilMatcherFunc { actual, failureMessage throws in
+    failureMessage.expected = "expected signal"
+    failureMessage.postfixMessage = "complete"
+    failureMessage.actualValue = nil
+
+    let signal: Signal<Value, Error>! = try actual.evaluate()
+
+    var completed = false
+    signal.observeCompleted {
+      completed = true
+    }
+    return completed
+  }
+}
+
 public func complete<Value, Error: ErrorType>() -> NonNilMatcherFunc<SignalProducer<Value, Error>> {
   return NonNilMatcherFunc { actual, failureMessage throws in
     failureMessage.expected = "expected producer"
--- Tests/CompleteMatcherSpec.swift
+++ Tests/CompleteMatcherSpec.swift
@@ -7,7 +7,14 @@ final class CompleteMatcherSpec: FailureRecordingSpec {
   override func spec() {
 
     describe("complete()") {
-      it("fails if the signal producer doesn't complete") {
+      it("fails if a signal doesn't complete") {
+        let (signal, _) = Signal<Int, NoError>.pipe()
+        expect {
+          expect(signal).to(complete())
+        }.to(recordFailure("failed - expected signal to complete"))
+      }
+
+      it("fails if a signal producer doesn't complete") {
         let producer: SignalProducer<Int, NoError> = .never
         expect {
           expect(producer).to(complete())
